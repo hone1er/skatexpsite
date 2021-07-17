fc03a924c9f254e2391b0fa07baa4883cf25affa
Joe <josephmvillavicencio@gmail.com>
2021-07-17 12:52:43 -0700

    statiCC

--- skatexp/settings.py
+++ skatexp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['skatexp.herokuapp.com', "127.0.0.1"]
 
 # email setup
 SENDGRID_API_KEY = config('SENDGRID_API_KEY')
