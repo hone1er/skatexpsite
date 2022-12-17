123da0454bc2cc4e7cab21b4615e84e20bb82cec
hone1er <24376928+hone1er@users.noreply.github.com>
2022-12-17 10:25:09 +0100

    migrate test

--- staff/models.py
+++ staff/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+
 # Create your models here.
 class Staff(models.Model):
-    first_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=101)
     last_name = models.CharField(max_length=100)
     content = RichTextField(blank=True, null=True)
-    image = models.ImageField(upload_to='images', blank=True)
+    image = models.ImageField(upload_to="images", blank=True)
 
     def __str__(self):
         return self.last_name
