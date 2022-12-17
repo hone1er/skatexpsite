594fea7cea16295c1c56df7b2563dfd45035e38a
hone1er <24376928+hone1er@users.noreply.github.com>
2022-12-17 10:56:01 +0100

    migratez'

--- staff/models.py
+++ staff/models.py
@@ -3,8 +3,8 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Staff(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75)
     content = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to="images", blank=True)
 
